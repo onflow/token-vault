7497b9ab555ee880b6825865395d5e952cf84cc9
Lea Lobanov <lea.lobanov@icloud.com>
2024-12-23 22:06:39 +0300

    Simplify recipe fields

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 // Pass the repo name
 const recipe = "token-vault";
 
-//Generate paths of each code file to render
-const contractPath = `${recipe}/cadence/contract.cdc`;
-const transactionPath = `${recipe}/cadence/transaction.cdc`;
-
-//Generate paths of each explanation file to render
-const smartContractExplanationPath = `${recipe}/explanations/contract.txt`;
-const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
-
 export const tokenVault = {
   slug: recipe,
   title: "Token Vault",
@@ -18,10 +10,6 @@ export const tokenVault = {
     "https://play.onflow.org/8b168390-9712-4578-a437-d15125cf18d9",
   excerpt:
     "This explains the vault resource that can be created in order for you to handle fungible tokens.",
-  smartContractCode: contractPath,
-  smartContractExplanation: smartContractExplanationPath,
-  transactionCode: transactionPath,
-  transactionExplanation: transactionExplanationPath,
   filters: {
     difficulty: "beginner",
   },
